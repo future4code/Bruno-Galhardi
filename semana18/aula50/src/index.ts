779ad3c003510d7e5cba78cc562509d91390f5ae
Bruno-Galhardi <brunogvallim@gmail.com>
2021-04-06 14:58:46 -0300

    exercicio completo

--- semana18/aula50/src/index.ts
+++ semana18/aula50/src/index.ts
@@ -1,8 +1,11 @@
 import app from './app'
 import getUserByEmail from './endpoints/Gets/getUsersByEmail'
+import getUserById from './endpoints/Gets/getUsersById'
 import createUser from './endpoints/Posts/createUser'
+import login from './endpoints/Posts/login'
 
 
-
+app.get('/user/profile', getUserById)
 app.get('/user/:email', getUserByEmail)
 app.post('/user/create', createUser)
+app.post('/user/login', login)
